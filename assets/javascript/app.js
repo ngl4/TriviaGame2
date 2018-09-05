4de4aad7f244ac9c56511b230b01c380fdf91569
Binh Lam <cindybinh444@gmail.com>
2018-09-05 17:26:20 -0400

    add pseudo-code

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+//START button 
+//show at the beginning 
+//hide the button when it is clicked 
+//show the timer and the first question 
+//Timer Starts decremeting from 10 seconds 
+//if it gets to zero, the answer will display === same as the submit button is clicked
+
+//QUESTIONS  (same for each question)
+//show with the timer starts 
+//includes a SUBMIT button
+
+//SUBMIT button 
+//when the submit button is clicked, it will compare the answers 
+//the timer stops 
+//save up the answers (for final display of all the correct/incorrect/unselected answers)
+//if it is a correct answer, display text- congratualtion, display image, and display the correct answer
+//if it is incorrect/unselected, display text- Great try, but.../ You are not sure?, display image, and display correct answer
+//a NEXT button to go to the next question 
+
+//NEXT button 
+//show second question
+//timer starts 
+//includes submit button 
+
+//When we get to the last question, after submitting the answer, 
+//there is no next button; replace it with SHOW RESULTS button 
+
+//SHOW RESULTS button 
+//display final-answer id 
+//includes a RESTART button to restart the entire game
+
+
+
